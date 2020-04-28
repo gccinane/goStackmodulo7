ef77007fc2870fd90453ff97630438dab70a04eb
JOSE <gccinane@gmail.com>
2020-04-27 22:56:21 -0300

    aplicação finalizada com navegação no sagas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import Header from './Components/Header';
 import GlobalStyle from './styles/global';
 import store from './store';
+import history from './services/history';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
-      </BrowserRouter>
+        <ToastContainer autoCLose={3000} />
+      </Router>
     </Provider>
   );
 }
