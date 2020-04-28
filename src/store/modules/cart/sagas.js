14143eecf86e974e5b112950f5d4f804db1ed9ba
JOSE <gccinane@gmail.com>
2020-04-27 22:17:54 -0300

    middleware com sagas adicionado

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,25 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, select, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSuccess } from './reducer';
+import { formatPrice } from '../../../util/format';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const res = yield call(api.get, `/products/${id}`);
+  const productExists = yield select((state) =>
+    state.cart.find((p) => p.id === id)
+  );
 
-  yield put(addToCartSuccess(res.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const res = yield call(api.get, `/products/${id}`);
+    const data = {
+      ...res.data,
+      amount: 1,
+      priceFormatted: formatPrice(res.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
